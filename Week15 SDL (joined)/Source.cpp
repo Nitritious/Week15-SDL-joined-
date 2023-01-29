d0b5ca92c45b3354dd72360a7d2574e2893d9b84
Zehui2020 <zehui2020@gmail.com>
2023-01-29 21:51:29 +0800

    Update Source.cpp

--- Week15 SDL (joined)/Source.cpp
+++ Week15 SDL (joined)/Source.cpp
@@ -2,11 +2,8 @@
 using namespace std;
 
 int main() {
-<<<<<<< Updated upstream
 	cout << "Hello, this is Yicheng\n";
 	cout << "Hello, this is Robbery\n";
-=======
 	cout << "Hello this is Yicheng\n";
 	cout << "Hello this is Zehui\n";
->>>>>>> Stashed changes
 }
